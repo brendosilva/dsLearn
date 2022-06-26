98d3db6a9a4d7dac189235e776907ca46fd9123d
Brendo Silva <breendo.01@gmail.com>
2022-06-25 22:24:25 -0300

    reply

---
+++ src/main/java/com/devsuperior/dslearnbds/entities/Reply.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dslearnbds.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_replay")
+public class Replay implements Serializable {
+}
