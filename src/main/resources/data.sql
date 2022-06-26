fb05e6ae1fd175d9d20932db1652c367976b4294
Brendo Silva <breendo.01@gmail.com>
2022-06-25 21:54:32 -0300

    seed lessons, e lessonsDone

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -38,3 +38,20 @@ INSERT INTO tb_section (title, description, position, image_Uri, resource_id, pr
 
 INSERT INTO tb_enrollment(user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2020-06-09T03:00:00.12345Z', null, true, false );
 INSERT INTO tb_enrollment(user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2020-06-09T03:00:00.12345Z', null, true, false );
+
+
+INSERT INTO tb_lesson(title, position, section_id) VALUES ('aula 1 capitulo 1', 1, 1);
+INSERT INTO tb_content(text_Content, video_Url , id) VALUES ('Material de apoio', 'https://www.youtube.com/watch?v=wzJTpzHHoVg', 1 );
+
+INSERT INTO tb_lesson(title, position, section_id) VALUES ('aula 2 capitulo 1', 2, 1);
+INSERT INTO tb_content(text_Content, video_Url, id) VALUES ('', 'https://www.youtube.com/watch?v=wzJTpzHHoVg', 2 );
+
+INSERT INTO tb_lesson(title, position, section_id) VALUES ('aula 1 capitulo 1', 3, 1);
+INSERT INTO tb_content(text_Content, video_Url, id) VALUES ('', 'https://www.youtube.com/watch?v=wzJTpzHHoVg', 3 );
+
+
+INSERT INTO tb_lesson(title, position, section_id) VALUES ('TAREFA 1 capitulo 1', 4, 1);
+INSERT INTO tb_task(id, description, question_Count,approval_Count, weight, due_Date) VALUES (4, 'API RESTFULL', 5, 4, 1.0, TIMESTAMP WITH TIME ZONE '2020-06-25T03:00:00.12345Z' );
+
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (1, 1, 1);
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (2, 1, 1);
